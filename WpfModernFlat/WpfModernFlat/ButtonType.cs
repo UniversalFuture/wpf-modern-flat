99c67ea8f851827f8ddf53cf694ba4d000539f1e
regiostech <ultralammed@yahoo.com>
2015-06-13 23:34:26 -0400

    Let's get it started, ha!

---
+++ WpfModernFlat/WpfModernFlat/ButtonType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfModernFlat
+{
+    /// <summary>
+    /// The various types of modern flat buttons.
+    /// </summary>
+    public enum ButtonType
+    {
+        Default,
+        Info,
+        Danger,
+        Warning,
+        Success,
+        Primary
+    }
+}
---
+++ WpfModernFlat/WpfModernFlat/Controls/ModernButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfModernFlat.Controls
+{
+    /// <summary>
+    /// A modern button!
+    /// </summary>
+    public partial class ModernButton : Button
+    {
+        public ButtonType Type
+        {
+            get { return (ButtonType)GetValue(TypeProperty); }
+            set { SetValue(TypeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Type.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.Register("Type", typeof(ButtonType), typeof(ModernButton), new PropertyMetadata(ButtonType.Default));
+
+
+        public ModernButton()
+        {
+        }
+    }
+}
